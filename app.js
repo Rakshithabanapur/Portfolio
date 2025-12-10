3e867a490830760068fd4099b30bfb7c097bd311
Rakshitha Banapur <rakshithabanapur29@gmail.com>
2025-12-11 00:54:36 +0530

    added rakshi project images

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
-   window.addEventListener("DOMContentLoaded", () => {
-        const dots = document.querySelectorAll(".dot-wrapper");
+window.addEventListener("DOMContentLoaded", () => {
+    const dots = document.querySelectorAll(".dot-wrapper");
 
-        dots.forEach(dot => dot.classList.add("show-tooltip"));
+    dots.forEach(dot => dot.classList.add("show-tooltip"));
+    const second = 1000;
+    setTimeout(() => {
+        dots.forEach(dot => dot.classList.remove("show-tooltip"));
+    }, 20 * second);
 
-        setTimeout(() => {
-            dots.forEach(dot => dot.classList.remove("show-tooltip"));
-        }, 60000);
+    const cardDotMap = {
+        rakshi: document.querySelector(".dot.pink").closest(".dot-wrapper"),
+        gnani: document.querySelector(".dot.blue").closest(".dot-wrapper")
+    };
+
+    document.querySelectorAll(".card").forEach(card => {
+        card.addEventListener("mouseenter", () => {
+            if (card.classList.contains("rakshi")) {
+                cardDotMap.rakshi.classList.add("show-tooltip");
+            }
+            if (card.classList.contains("gnani")) {
+                cardDotMap.gnani.classList.add("show-tooltip");
+            }
+        });
+
+        card.addEventListener("mouseleave", () => {
+            if (card.classList.contains("rakshi")) {
+                cardDotMap.rakshi.classList.remove("show-tooltip");
+            }
+            if (card.classList.contains("gnani")) {
+                cardDotMap.gnani.classList.remove("show-tooltip");
+            }
+        });
     });
-
+});
